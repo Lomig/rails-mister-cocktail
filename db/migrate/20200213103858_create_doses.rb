16f20702f3b1dfdef66168be3cfb036b592817c2
Lomig <dunwael@gmail.com>
2020-02-13 11:57:51 +0100

    Fix migration class (mispell)

--- db/migrate/20200213103858_create_doses.rb
+++ db/migrate/20200213103858_create_doses.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class CreateDoses < ActiveRecord::Migration[6.0]
   def change
     create_table :doses do |t|
       t.string :description
-      t.references :Cocktail, null: false, foreign_key: true
-      t.references :Ingredient, null: false, foreign_key: true
+      t.references :cocktail, null: false, foreign_key: true
+      t.references :ingredient, null: false, foreign_key: true
 
       t.timestamps
     end
