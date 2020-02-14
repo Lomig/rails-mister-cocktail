68b574a19961763c0aaea4068771a88f5c6d2da4
Lomig <dunwael@gmail.com>
2020-02-14 15:39:47 +0100

    Fix card flip + Add Custom Image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_115102) do
+ActiveRecord::Schema.define(version: 2020_02_14_115350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 2020_02_14_115102) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "instructions"
-    t.string "image"
   end
 
   create_table "doses", force: :cascade do |t|
