42cab09805f7af704838f2e47961d21a496b68a7
Lomig <dunwael@gmail.com>
2020-02-15 00:00:43 +0100

    Improving Ingredient card

--- app/javascript/custom/showForm.js
+++ app/javascript/custom/showForm.js
@@ -1,7 +1,3 @@
 window.showIngredientsForm = () => {
-  // document.getElementById("hidden-form").classList.remove("hide");
-  // document.getElementById("hidden-form").classList.add("show");
-  // document.getElementById("button-hide").classList.remove("show");
-  // document.getElementById("button-hide").classList.add("hide");
   document.getElementById("flip-ingredients").classList.toggle("is-flipped");
 }
